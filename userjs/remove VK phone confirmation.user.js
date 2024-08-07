18f4896f6760e101d94cb9ded18b4b4f652b9d6e
shrek-gabe-itch <58945139+shrek-gabe-itch@users.noreply.github.com>
2024-08-07 22:00:49 +0500

    vk

---
+++ userjs/remove VK phone confirmation.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name			Remove VK phone confirmation popup
+// @namespace		Violentmonkey Scripts
+// @version			3.0.0
+// @description		Kak je ya vas nenaviju dermoedi
+// @author			Thrower
+// @match			https://*.vk.com/*
+// @icon			https://www.google.com/s2/favicons?sz=64&domain=vk.com
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	const POPUP_CLASS = "vkuiPopoutWrapper";
+	const VALIDATION_CLASS_PREFIX = "CheckValidationPhone";
+
+	function removeElement(element) {
+		if (element && element.parentNode) {
+			element.parentNode.removeChild(element);
+		}
+	}
+
+	function clearBodyStyles() {
+		document.body.removeAttribute("style");
+	}
+
+	function containsValidationClass(node) {
+		if (node.nodeType === 1 && node.classList) {
+			for (const className of node.classList) {
+				if (className.startsWith(VALIDATION_CLASS_PREFIX)) {
+					return true;
+				}
+			}
+		}
+		for (const childNode of node.childNodes) {
+			if (containsValidationClass(childNode)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function handleChanges(mutationsList) {
+		for (const mutation of mutationsList) {
+			if (mutation.type === "childList") {
+				for (const addedNode of mutation.addedNodes) {
+					if (addedNode.nodeType === 1 && addedNode.classList.contains(POPUP_CLASS)) {
+						if (containsValidationClass(addedNode)) {
+							console.debug("VK PHONE CONFIRM:", "Remove element", `"${addedNode.className}"`)
+							removeElement(addedNode);
+							clearBodyStyles();
+						}
+					}
+				}
+			}
+		}
+	}
+
+	const observer = new MutationObserver(handleChanges);
+	const config = { childList: true, subtree: true };
+
+	observer.observe(document.body, config);
+})();
